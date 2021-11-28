25bca80ffac862f67f47f3af4729f6624a5d66ae
Vano <ivan@work5.ru>
2021-11-28 21:51:07 +0700

    clean install test

--- docker/build.sh
+++ docker/build.sh
@@ -7,10 +7,10 @@ mkdir -m 777 ../../docker_data/tmp
 mkdir -m 777 ../../docker_data/logs
 mkdir -m 777 ../../docker_data/logs/nginx
 mkdir -m 777 ../../docker_data/logs/php
-chmod -R 0777 ../web/assets
+chmod -R 777 ../web/assets
 
 docker-compose up -d --build
-sleep 1
+sleep 5
 docker exec wwp_db mysql -uroot -prootpwd --init-command="CREATE DATABASE IF NOT EXISTS wwp"
 docker exec wwp_php composer install
 docker exec wwp_php php yii cron/initdb
